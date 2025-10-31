fa99c411e78e51a82961da1977cd310680a1896c
Vishwas Gowda P N <mechdoctor231@gmail.com>
2025-10-31 14:17:48 +0100

    Prune large archive and build/log caches; add external/ README with Drive link

--- axiom_ws/build/axiom_base_bridge/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ing-vishwa/Desktop/Axiom_Robotics/axiom_ws/install/axiom_base_bridge'
---
+++ external/robotic_arm_fabricreator/README.md
@@ -0,0 +1,2 @@
+Fabricreator controller downloads:
+https://drive.google.com/drive/folders/155aWvKH5jcQ96tFvs-LeDclDERoTMNEt
